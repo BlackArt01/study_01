12e19b98dd1076f4a3ddd49435de3309ed872d44
BlackArt01 <baek0337dev@gmail.com>
2025-12-24 17:00:51 +0900

    Add 중첩 라우팅.

--- frontend-react/src/router/AppRouter.jsx
+++ frontend-react/src/router/AppRouter.jsx
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from '../layouts/MainLayout';
 import TasksPage from '../pages/TasksPage';
 import StatsPage from '../pages/StatsPage';
 import SettingsPage from '../pages/SettingsPage';
-import Navbar from '../components/Navbar';
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<TasksPage />} />
-        <Route path="/stats" element={<StatsPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
+        {/* Layout Route */}
+        <Route path="/" element={<MainLayout />}>
+          {/* Index Route */}
+          <Route index element={<TasksPage />} />
+
+          {/* Child Routes */}
+          <Route path="stats" element={<StatsPage />} />
+          <Route path="settings" element={<SettingsPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
